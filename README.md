f8137242dcb7fd9ad10724af87361fbf5c0437e9
cmempalmado <cmempalmado@gmail.com>
2023-04-18 00:07:59 -0700

    finished

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ SO THAT I can ease my mind for the stress I will face
 
 
 - - -
-Copyright © [2022] [Christian Empalmado]
+Copyright © [2023] [Christian Empalmado]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- script.js
+++ script.js
@@ -1,59 +1,63 @@
-const wordguess = document.getElementById('guessword');
-
-
-
-// Define an array of words to be guessed
-const words = ["javascript", "html", "css", "nodejs"];
+// Define the words to be guessed
+const words = ["harry potter", "star wars", "lord of the rings", "song of ice and fire"];
 
 // Choose a random word from the array
 const randomIndex = Math.floor(Math.random() * words.length);
-const word = words[randomIndex];
+let word = words[randomIndex];
 
 // Convert the word to an array of letters
-const wordArray = word.split("");
+let wordArray = word.split("");
 
 // Create an array of underscores to represent the hidden word
-const hiddenArray = wordArray.map(() => "_");
+let hiddenArray = wordArray.map(char => char === ' ' ? ' ' : '_');
 
 // Keep track of the number of incorrect guesses
 let wrongGuesses = 0;
 
-// Function to check if a letter is in the word
-function checkLetter(letter) {
+// Get DOM elements
+const wordContainer = document.getElementById("word-container");
+const guessesContainer = document.getElementById("guesses");
+const guessForm = document.getElementById("guess-form");
+const guessInput = document.getElementById("guess-input");
+
+// Function to update the hidden word and check if the game is over
+function updateGame(guess) {
   let found = false;
   for (let i = 0; i < wordArray.length; i++) {
-    if (wordArray[i] === letter) {
-      hiddenArray[i] = letter;
+    if (wordArray[i] === guess) {
+      hiddenArray[i] = guess;
       found = true;
     }
   }
   if (!found) {
     wrongGuesses++;
+    guessesContainer.textContent += guess + " ";
+  }
+  wordContainer.textContent = hiddenArray.join(" ");
+  if (wrongGuesses >= 6 || !hiddenArray.includes("_")) {
+    endGame();
   }
 }
 
-// Function to check if the game is over
-function checkGameOver() {
-  return wrongGuesses >= 6 || !hiddenArray.includes("_");
+// Function to end the game
+function endGame() {
+  guessInput.disabled = true;
+  guessInput.value = "";
+  guessInput.placeholder = "Game over!";
+  guessInput.style.backgroundColor = "gray";
+  guessInput.style.color = "white";
 }
 
-// Function to display the current state of the game
-function displayGame() {
-  console.log(`Word: ${hiddenArray.join(" ")}`);
-  console.log(`Incorrect guesses: ${wrongGuesses}`);
-}
-
-// Start the game
-while (!checkGameOver()) {
-  displayGame();
-  const guess = prompt("Guess a letter:").toLowerCase();
-  checkLetter(guess);
-}
-
-// End the game
-displayGame();
-if (wrongGuesses >= 6) {
-  console.log("You lose!");
-} else {
-  console.log("You win!");
-}
+// Event listener for the guess form
+guessForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let guess = guessInput.value.toLowerCase();
+  if (guess.length !== 1) {
+    alert("Please enter a single letter.");
+  } else if (!/[a-z]/.test(guess)) {
+    alert("Please enter a letter from a to z.");
+  } else {
+    updateGame(guess);
+  }
+  guessInput.value = "";
+});
